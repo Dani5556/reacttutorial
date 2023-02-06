7e27670da10b9d995912abf94799a114e35ae501
Dani5556 <danynago74@gmail.com>
2023-02-06 19:01:03 +0200

    signinstuff

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,23 +1,49 @@
 import { GetServerSideProps } from 'next';
 import {signIn, getSession, getCsrfToken, getProviders} from 'next-auth/react';
 import { redirect } from 'next/dist/server/api-utils';
+import styles from '../styles/signin.module.scss';
+
+function ProviderButton({ provider }) {
+    switch (provider.name) {
+      case 'Google':
+        return (
+          <img
+            src="https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png"
+            alt="Sign in with Google"
+          />
+        );
+      case 'Facebook':
+        return (
+          <img
+            src="https://www.facebook.com/images/fb_icon_325x325.png"
+            alt="Sign in with Facebook"
+          />
+        );
+      default:
+        return <div>Sign in with {provider.name}</div>;
+    }
+  }
+
 
 const inSign = ({providers}) => {
     
     return ( 
-      <div>
+    <div className={styles.cont}>
+      <div className={styles.doboz}>
       
-
+        <h2 className={styles.text}>Sign In With: </h2>
             {Object.values(providers).map((provider) => {
                 return(
                     <div key={provider.name}>
                         <button onClick={()=> signIn(provider.id)}>
-                            Sign in with {provider.name}
+                        <ProviderButton key={provider.name} provider={provider} />
                         </button>
+                        
                     </div>
                 );
             })}
         </div>
+    </div>
      );
 }
  
